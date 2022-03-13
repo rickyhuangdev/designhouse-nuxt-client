af8de165b7fba50f3803b4249f0dc06a10154f5d
Ricky <31544746+RickyHuang-tech@users.noreply.github.com>
2022-03-13 16:20:53 +0800

    Install base packages and setup the folder structure

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,12 +6,16 @@ export default {
       lang: 'en',
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: ''},
+      {name: 'format-detection', content: 'telephone=no'},
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}],
+    script: [
+      '~/assets/js/jquery.min',
+      '~/assets/js/bootstrap.bundle.min',
+    ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -24,7 +28,9 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [],
+  buildModules: [
+    '@nuxtjs/composition-api/module'
+  ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
@@ -33,13 +39,21 @@ export default {
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
   ],
+  bootstrapVue: {
+    // Install the `IconsPlugin` plugin (in addition to `BootstrapVue` plugin)
+    icons: true
+  },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: process.env.API_BASE,
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+  env: {
+    baseUrl: process.env.BASE_URL || 'http://localhost:3000'
+  }
+
 }
---
+++ pages/user/index.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="container">
+  <h3 class="">
+    sdf
+
+  </h3>
+</div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
